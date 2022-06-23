ba00c6cfe608babf3a4b9b0cca3f9ed1d92d5d03
Ivan Egorov <www.Len244@gmail.com>
2022-06-23 23:50:24 +0300

    Настройки для деплоя

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react()]
+  integrations: [preact(), react()],
+  site: "https://Leniorko.github.io/",
+  base: "Leniorko.github.io",
 });
