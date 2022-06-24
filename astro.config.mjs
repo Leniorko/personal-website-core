d2ceff1cc94a22ce3835890eea2c468e46ee8ab8
Ivan Egorov <www.Len244@gmail.com>
2022-06-24 17:21:49 +0300

    Fix #2 and some test deploy changes that i will try now

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), react()],
-  site: "https://Leniorko.github.io/",
+  site: "https://Leniorko.github.io/personal-website-core/",
   base: "/personal-website-core",
 });
