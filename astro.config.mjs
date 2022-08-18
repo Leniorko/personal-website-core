da3c0198378312506eeaa9e599a102b82f490daf
Ivan Egorov <www.Len244@gmail.com>
2022-08-18 23:31:52 +0300

    Updated all md to mdx. Correctly added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react(), tailwind()],
+  integrations: [preact(), react(), tailwind(), mdx()],
   site: "https://leniorko.github.io",
   base: "/personal-website-core",
-  legacy: {
-    astroFlavoredMarkdown: true,
-  },
 });
